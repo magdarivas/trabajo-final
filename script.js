91b6d81f3ff51bf8ed53ed54382d4dda91fd8e52
magdarivas <70529545+magdarivas@users.noreply.github.com>
2026-01-25 16:18:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,10 +12,11 @@ const projectsData = {
             "Colección de seis cinturones tipo faja con bordados inspirados en La Pincoya y La Añañuca. Producción 100% artesanal",
         challenge:
             "Propuesta realizada como entrega final individual de mi carrera. Con la opción de un ámbito del diseño libre, decidí realizarla en torno al bordado, una práctica a la cual le tengo especial cariño y poseo una amplia experiencia.",
-        solution:
+        solution: [
             "El bordado es una práctica capaz de narrar historias, y en los últimos años, ha crecido signicativamente su interés. Sin embargo, sigue habiendo una escasvalorización del trabajo implicado por parte del público general. Además, existe un predominio de influencias extranjeras y tendencias globalizadas, mientras que la representación de relatos locales son reiteradas y muy acotadas. ",
             "En búsqueda de un sello distintivo en una práctica donde las posibilidades son casi infinitas, se desarrolló una metología propia basada en la investigación y la experimentación. Tras varias iteraciones materiales y compositivas, se cayó en cuenta que estos no bastarían por sí solos. Luego de una búsqueda de relatos locales poco difundidos, se quiso poner en valor a las mitologías chilenas, cuya propia sociedad casi no recuerda o reconoce.",
             "Colección de cinturones tipo faja inspirados en La Pincoya y La Añañuca, mitos del sur y del norte de Chile respectivamente. De manera intencional, se elgieron dos personajes femeninos por la fuerte relación simbólica entre mujer y bordado, con sus diversas connotaciones a lo largo de la historia. Es una propuesta que resulta como punto de inicio para poner en valor el bordado y los relatos locales.",
+       ],
         galleryImages: [
             "img/img/XXXX.jpg",
             "img/img/XXXX.jpg",
